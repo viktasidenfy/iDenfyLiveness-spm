97e6e638e07183ea8ddf4c86a427a75c55a3bd75
J. Doe (https://devcenter.bitrise.io/builds/setting-your-git-credentials-on-build-machines/) <please-set-your-email@bitrise.io>
2024-09-25 09:23:54 +0000

    new version

--- Package.swift
+++ Package.swift
@@ -3,15 +3,15 @@
 
 import PackageDescription
 
-let version = "8.5.5"
+let version = "8.5.4"
 
 enum Checksums {
-    static let iDenfyInternalLoggerChecksum = "f66477b3edfe0417e169c973e2092b05d34d7f59ff7b55da1b225dc1fbe4e70b"
-    static let FaceTecSDKChecksum = "2204630cf79b71beb990e87e6f01aa16fcb19415eac8559eedcbeed747d8992e"
-    static let iDenfyLivenessChecksum = "c38c47b1599da8efa5501b76fb9a3324115018fe93fa859733fe14305428ef97"
-    static let idenfyviewsChecksum = "8d96d8780b16f27c24e742af6d024e71cdb48e304c7185b4be0d9664f3c09faf"
-    static let iDenfySDKChecksum = "029365ea601a4618ed45cdf006c0caaf44223799c49276b17e051f823bd6971e"
-    static let idenfycoreChecksum = "2592409eaf0b84081d5fd2795fd9e0dc0e86fafb5c54bfe13e88818db522bdc4"
+    static let iDenfyInternalLoggerChecksum = "2e80ffa44d8da6ec21f58970620fcdccb77be25bdb3d6a7811f3bc20ee90e56c"
+    static let FaceTecSDKChecksum = "5ba9eb1f482ca390e36b5eed4916dc3d12635db990e880ce9983fcbe96ead50d"
+    static let iDenfyLivenessChecksum = "2ef951e10e7a80d5376cecdd30cd616de2e1099bb60deb7690bc9f16b43dcf2c"
+    static let idenfyviewsChecksum = "a7f173c48cde990c7c2ea105a1dd56d8a3bffbffb50b9bc21be2423356db4220"
+    static let iDenfySDKChecksum = "089a69ece13a552c45ee47c10b8da3f696e33e1265d8442bc7eda3f9247c8337"
+    static let idenfycoreChecksum = "722949fe9d076b26208fcd84871a9abe560415494d7c37e6fbb2199186b17603"
 }
 
 let package = Package(
